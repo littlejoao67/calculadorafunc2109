a8dbfde7b4005fdfd42e213cbfed3d1955f226ee
unknown <lab1055@etecjk.local>
2026-09-21 21:40:34 -0300

    consegui fazer ebaa

--- resultado.php
+++ resultado.php
@@ -1,5 +1,4 @@
 <?php
-
         $num1 = $_POST['num1'];
         $num2 = $_POST['num2'];
         $operacao = $_POST['operacao'];
@@ -7,6 +6,44 @@
 
         function multiplicacao($num1, $num2){
             $resultado= $num1 * $num2;
-            echo "<h3>Resultado da multiplicação: $resultado </h3>";
+            return $resultado;
         }
+
+        function adicao($num1, $num2){
+            $resultado= $num1 + $num2;
+            return $resultado;
+        }
+
+        function subtracao($num1, $num2){
+            $resultado= $num1 - $num2;
+            return $resultado;
+        }
+
+        function divisao($num1, $num2){
+            $resultado= $num1 / $num2;
+            return $resultado;
+        }
+
+    if ($operacao == "*"){
+        $resultado = multiplicacao($num1, $num2);
+        echo "<h2>Sua multiplicação deu: $resultado </h2>";
+    }
+
+
+    if ($operacao == "+"){
+        $resultado = adicao($num1, $num2);
+        echo "<h2>Sua adição deu: $resultado </h2>";
+    }
+
+    
+    if ($operacao == "-"){
+        $resultado = subtracao($num1, $num2);
+        echo "<h2>Sua subtração deu: $resultado </h2>";
+    }
+
+    
+    if ($operacao == "/"){
+        $resultado = divisao($num1, $num2);
+        echo "<h2>Sua divisão deu: $resultado </h2>";
+    }
 ?>
